f22283d820ce5b07e0e2bdbd83ac8b1a9e947a4c
johnyshaik <jbshaik82@gmail.com>
2017-10-17 21:26:29 -0400

    JSON-P::JsonObject:: Write Json data to a file
    	Formatted code for readability.

--- JsonObject/MyJsonPObjectPractice/src/main/java/com/jsonp/object/example/JsonPObjectWriter.java
+++ JsonObject/MyJsonPObjectPractice/src/main/java/com/jsonp/object/example/JsonPObjectWriter.java
@@ -15,10 +15,16 @@ import javax.json.JsonWriter;
 public class JsonPObjectWriter {
 
 	public static void main(String[] args) {
-		JsonObject orderObject = new JsonPObjectWriter().buildPurchaseOrder();
+		JsonPObjectWriter objectWriter = new JsonPObjectWriter();
+		JsonObject orderObject = objectWriter.buildPurchaseOrder();
 		// Write to console
 		System.out.println("Order::\n" + orderObject);
 
+		// write to file.
+		objectWriter.writeToFile(orderObject);
+	}
+
+	public void writeToFile(JsonObject orderObject) {
 		File orderFile = Paths.get("src/main/resources/order.json").toFile();
 		if (orderFile.exists()) {
 			orderFile.delete();
@@ -32,28 +38,30 @@ public class JsonPObjectWriter {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 	}
 
 	public JsonObject buildPurchaseOrder() {
 
+		// Customer Details
 		JsonObjectBuilder customerBuilder = Json.createObjectBuilder().add("@first_name", "johny")
 				.add("@last_name", "shaik").add("email", "johnyzhub@github.com").add("phoneNumber", "1456345789");
 
-		JsonObjectBuilder lineItemOneBuilder = Json.createObjectBuilder();
-		lineItemOneBuilder.add("@item", "H2G2").add("@quantity", "1").add("unit_price", "23.5");
-		JsonObjectBuilder lineItemTwoBuilder = Json.createObjectBuilder();
-		lineItemTwoBuilder.add("@item", "Matrix").add("@quantity", "2").add("unit_price", "32.5");
+		// Line Order Details
+		JsonObjectBuilder lineItemOneBuilder = Json.createObjectBuilder().add("@item", "H2G2").add("@quantity", "1")
+				.add("unit_price", "23.5");
+		JsonObjectBuilder lineItemTwoBuilder = Json.createObjectBuilder().add("@item", "Matrix").add("@quantity", "2")
+				.add("unit_price", "32.5");
 		JsonArrayBuilder lineArrayBuilder = Json.createArrayBuilder().add(lineItemOneBuilder).add(lineItemTwoBuilder);
 		JsonObjectBuilder orderLineBuilder = Json.createObjectBuilder().add("order_line", lineArrayBuilder);
 
+		// Credit Card Details
 		JsonObjectBuilder creditCardBuilder = Json.createObjectBuilder().add("@number", "1234")
 				.add("@expiry_date", "10/13").add("@control_number", "465").add("@type", "visa");
 
+		// Order Details
 		JsonObjectBuilder orderBuilder = Json.createObjectBuilder().add("@id", "1234").add("@date", "10/16/2017")
 				.add("customer", customerBuilder).add("content", orderLineBuilder)
 				.add("credit_card", creditCardBuilder);
 		return orderBuilder.build();
 	}
-
 }
