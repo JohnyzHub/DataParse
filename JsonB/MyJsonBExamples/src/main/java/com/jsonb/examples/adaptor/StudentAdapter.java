4bbb51e957c27f76f4bb9e53a3e3358ee7e827c8
johnyshaik <jbshaik82@gmail.com>
2017-10-19 23:11:16 -0400

    JSON-B:: Adapter Class

---
+++ JsonB/MyJsonBExamples/src/main/java/com/jsonb/examples/adaptor/StudentAdapter.java
@@ -0,0 +1,26 @@
+package com.jsonb.examples.adaptor;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.bind.adapter.JsonbAdapter;
+
+import com.jsonb.examples.Student;
+
+public class StudentAdaptor implements JsonbAdapter<Student, JsonObject> {
+
+	@Override
+	public JsonObject adaptToJson(Student student) throws Exception {
+		return Json.createObjectBuilder().add("id", student.getNumber()).add("firstName", student.getFirstName())
+				.add("lastName", student.getLastName()).add("middleName", student.getMiddleName())
+				.add("grade", student.getGrade()).build();
+	}
+
+	@Override
+	public Student adaptFromJson(JsonObject jsonObject) throws Exception {
+		Student student = new Student(jsonObject.getInt("id"), jsonObject.getString("firstName"),
+				jsonObject.getString("lastName"), jsonObject.getString("middleName"), jsonObject.getString("grade"));
+
+		return student;
+	}
+
+}
